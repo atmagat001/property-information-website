5f75e0de5a346eddcba8981adbbbf9e248baf079
atmagat001 <atmagat@gmail.com>
2023-03-20 20:44:57 +0800

    main commit

---
+++ script.js
@@ -0,0 +1,47 @@
+let http = new XMLHttpRequest();
+
+http.open('get', 'data.json', true);
+
+http.send();
+
+http.onload = function() {
+    if(this.readyState== 4 && this.status == 200) {
+        let listings = JSON.parse(this.responseText);
+        let output = "";
+       
+        for (let item of listings) {
+            output += `
+            <div class="listing">
+                
+                <img class="image-container" src="../images/${item.image}" alt="">
+                <div class="details-container">
+                        <p class="details name">${item.name}</p>
+                        <p class="details classification">Asset Classification: ${item.classification}</p>
+                        <p class="details">${item.address}</p>
+                        
+
+                        <p class="details">Valuation per sqm: Phpxxx</p>
+                        <p class="details">Zonal Value per sqm: Phpxxx</p>
+                        <p class="details">Total Asset Value: Phpxxx</p>
+
+                        <p class="details reco">Recommendation: Hold</p>
+
+
+                        <div class="links-container">
+                            <a href="${item.maplink}" class="details">Google maps link</a>
+                            <a href="${item.streetview}" class="details">Google Streetview</a>
+
+                      </div>
+
+                        
+                
+                  </div>
+                
+             </div>
+            
+            `
+        }
+        console.log(output);
+        document.getElementById("listings").innerHTML = output;
+    }
+}
